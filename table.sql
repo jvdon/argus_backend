6d6d122f799d05b306e2b9c296767bc5e25a54d0
Zeyk <joaovictordonato2@gmail.com>
2024-04-11 17:32:33 -0300

    UPDATED TABLES

--- table.sql
+++ table.sql
@@ -1,12 +1,16 @@
-CREATE TABLE reports (
-	id INT PRIMARY KEY auto_increment,
-    coords VARCHAR(255) NOT NULL,
-    image VARCHAR(255) NOT NULL,
-);
-
 CREATE TABLE users (
-	id INT PRIMARY KEY auto_increment,
+    id INT PRIMARY KEY auto_increment,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE `reports` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `coords` varchar(255) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    `userId` int DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `userid_idx` (`userId`),
+    CONSTRAINT `userid` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
